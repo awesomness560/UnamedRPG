543b14eda9a73a1e6a615eada65a2b11591fa455
awesomness560 <61993348+awesomness560@users.noreply.github.com>
2020-11-11 12:05:50 -0600

    Item Database

---
+++ UnamedRPG/Assets/Scripts/Item.cs
@@ -0,0 +1,35 @@
+﻿using MyBox;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item 
+{
+    public string name;
+    public GameObject prefab;
+    public Transform location;
+    public Rarity rarity;
+    public Type type;
+    [ConditionalField("type", false, Type.Melee)] public bool OverrideMeleeAnimation;
+    [ConditionalField("OverrideMeleeAnimation")] public Animation MeleeAnimation;
+    [ConditionalField("type", false, Type.Armor)] public bool OverrideMovementAnimation;
+    [ConditionalField("OverrideMovementAnimation")] public Animation MovementAnimation;
+
+
+    public enum Rarity
+    {
+        Common,
+        Rare,
+        Epic,
+        Mystical
+    }
+
+    public enum Type
+    {
+        Melee,
+        Armor,
+        Other
+    }
+}
---
+++ UnamedRPG/Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public Item[] items;
+    public void Equip(int arrayNumber)
+    {
+        var data = GameObject.FindGameObjectWithTag("ItemDatabase").GetComponent<ItemDatabase>();
+        GameObject.Instantiate(data.items[arrayNumber].prefab, data.items[arrayNumber].location);
+    }
+}
